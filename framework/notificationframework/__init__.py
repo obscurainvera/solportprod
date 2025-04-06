2c8025f4d82b597cf672e6e8cdaed394851ec019
henry.kiruba <henry.kiruba@zohocorp.com>
2025-04-06 20:52:10 +0530

    sd

--- framework/notificationframework/__init__.py
+++ framework/notificationframework/__init__.py
@@ -1,7 +1,1 @@
-from config.config import get_config
-"""
-Notification Framework Module
 
-Provides a modular framework for sending notifications across various platforms
-with a standardized interface and database persistence.
-""" 
