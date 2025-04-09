417fce20d96089f540f387e242bc360cbffc31e2
henry.kiruba <henry.kiruba@zohocorp.com>
2025-04-10 02:06:21 +0530

    fix

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+from app import create_app
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    app.run(host="0.0.0.0", port=5000)
+    app = create_app()
+    app.run(host="0.0.0.0", port=10000)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
